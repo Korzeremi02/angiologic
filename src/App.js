939388fdbf076a1b6e60c64b787ca7c22048d894
Rémi KORZENIOWSKI <r.korzeniowski@icloud.com>
2023-12-13 12:45:04 +0100

    fix(all): bugs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { fabric } from 'fabric';
+import './App.scss';
 
 function App() {
+  useEffect(() => {
+    const canvas = new fabric.Canvas('canvaSct', {
+      backgroundColor: '#fff',
+      width: 800,
+      height: 600,
+    });
+
+    canvas.on('mouse:down', (options) => {
+      if (options.target) return;
+      const rect = new fabric.Rect({
+        left: options.pointer.x,
+        top: options.pointer.y,
+        width: 50,
+        height: 50,
+        fill: 'red',
+      });
+      canvas.add(rect);
+    });
+  }, []); 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='AppHeader'></div>
+      <div className='AppContent'>
+        <div className='UserArea'>
+          <div className='InputArea'>
+            <div className='BtnArea'>
+              <input type='submit' value='Crayon'></input>
+              <input type='submit' value='Gomme'></input>
+              <input type='submit' value='Remplir'></input>
+              <input type='submit' value='Rectangle'></input>
+              <input type='submit' value='Télécharger'></input>
+            </div>
+          </div>
+          <div className='CanvaArea'>
+            <div className='CanvaSct' id='canvaSct'></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
